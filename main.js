185b2b3c087670512291a26f60f1b6acfa341345
Justin Schang <theschangsta@yahoo.com>
2023-08-03 19:59:00 -0400

    🙌👍 second commit added remove function/button added a read book checkbox

--- main.js
+++ main.js
@@ -6,17 +6,20 @@ const authorbox = document.getElementById("input-box-2");
 const pagesbox = document.getElementById("input-box-3");
 const parentDiv = document.getElementById("parent");
 const childDiv = document.getElementById("child");
+const removeBtn = document.querySelector(".remove");
+
 let myLibrary = [];
 
 class Book {
-    constructor(title, author, pages) {
+    constructor(title, author, pages, checkbox) {
         this.title = title;
         this.author = author;
         this.pages = pages;
+        this.checkbox = checkbox;
     }
 }
 
-// button onclick events
+// add new book onclick button
 formBtn.addEventListener("click", () => {
     formBtn.classList.add("visually-hidden");
     addNewBookForm.classList.remove("visually-hidden");
@@ -29,29 +32,71 @@ addNewBookBtn.addEventListener("click", () => {
     const title = titlebox.value;
     const author = authorbox.value;
     const pages = pagesbox.value;
-    const newBook = new Book(title, author, pages);
+    const checkbox = "";
+    const newBook = new Book(title, author, pages, checkbox);
     myLibrary.push(newBook);
     addNewBook();
     myLibrary.pop([newBook]);
 })
 
+// function to clear input boxes
+function inputClear() {
+    titlebox.value = "";
+    authorbox.value = "";
+    pagesbox.value = "";
+}
+
+// function to add new book to library array & webpage
 function addNewBook() {
     for (let i = 0; i < myLibrary.length; i++) {
         const book = myLibrary[i];
 
         let newDiv = document.createElement("div");
+        let readDiv = document.createElement("div");
+        let removeBtn = document.createElement("button");
         newDiv.setAttribute("class", "child");
+        removeBtn.setAttribute("class", "remove");
+        
+        let removeElement = document.createElement("p");
+        removeElement.textContent = "Remove Book"
 
         let titleElement = document.createElement("p");
-        titleElement.textContent = `Title: ${book.title}`;
+        titleElement.textContent = `TITLE: ${book.title}`;
 
         let authorElement = document.createElement("p");
-        authorElement.textContent = `Author: ${book.author}`;
+        authorElement.textContent = `AUTHOR: ${book.author}`;
 
         let pagesElement = document.createElement("p");
-        pagesElement.textContent = `Pages: ${book.pages}`;
+        pagesElement.textContent = `PAGES: ${book.pages}`;
 
-        newDiv.append(titleElement, authorElement, pagesElement);
+        // checkbox marking read or unread
+        let checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.name = "name";
+        checkbox.value = "value";
+        checkbox.id = "id";
+
+        let label = document.createElement('label')
+        label.htmlFor = "id";
+        label.appendChild(document.createTextNode('READ: '));
+
+        readDiv.append(label, checkbox);
+
+        // displays elements onto webpage
+        newDiv.append(titleElement, authorElement, pagesElement, removeElement);
+        removeBtn.append(removeElement);
         parentDiv.appendChild(newDiv);
+        newDiv.append(readDiv);
+        newDiv.appendChild(removeBtn);
+
+        inputClear();
+
+        // remove book function onclick
+        function removeBook() {
+            parentDiv.removeChild(newDiv);
+        }
+        removeBtn.addEventListener("click", () => {
+            removeBook();
+        })
     }
 }
